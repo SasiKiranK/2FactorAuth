3c7d798bf455f57f4bea590bce29ce66f4dd7899
prathap <prathap.g@innoppl.com>
2017-02-15 18:25:00 +0530

    success Observer temp completed

--- Commerceshop/TwoFactor/Observer/AdminLoginSucceeded.php
+++ Commerceshop/TwoFactor/Observer/AdminLoginSucceeded.php
@@ -7,24 +7,38 @@ class AdminLoginSucceeded implements ObserverInterface
 {
 	protected $_responseFactory;
 	protected $_url;
-	/*protected $_authSession;*/
+	protected $_authSession;
+	protected $_session;
 	public function __construct(\Magento\Framework\App\ResponseFactory $responseFactory,
-		\Magento\Framework\UrlInterface $url)
+		\Magento\Backend\Model\Auth\Session $authSession,
+		\Magento\Framework\UrlInterface $url,
+		\Magento\Backend\Model\Session $session)
 	{	
 		$this->_responseFactory = $responseFactory;
 		$this->_url = $url;
-		/*$this->_authSession = $authSession;*/
+		$this->_authSession = $authSession;
+		$this->_session = $session;
         
 	}
     public function execute(Observer $observer) 
     {
 
-    	/*$auth = $this->_authSession;
-    	$auth->getUser()->getUserId();*/
-    	$writer = new \Zend\Log\Writer\Stream(BP . '/var/log/TwoFactor.log'); 
-    	$logger = new \Zend\Log\Logger(); 
-    	$logger->addWriter($writer); 
-    	$logger->info("workz");
+    	$auth = $this->_authSession;
+    	$auth->getUser()->getUserId();
+    	$this->_session->setIsloggedin(1);
+    	$this->_session->setOtpdone(0);
+    	$this->_session->setOtp(1234);
+    	// echo $this->_session->getSasi();
+
+    	// $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/TwoFactor.log'); 
+    	// $logger = new \Zend\Log\Logger(); 
+    	// $logger->addWriter($writer); 
+    	// $logger->info($auth);
+    	// echo $auth->getUser()->getUserId();
+    	// exit();
          //$this->redirect->redirect($controller->getResponse(), 'exampleadminnewpage/helloworld/index');
     }
+
+
+    
 }
