7dd5bbb0d0026cf07bee167daf9deb0d99b083c7
prathap <prathap.g@innoppl.com>
2017-02-16 13:37:59 +0530

    block working

--- Commerceshop/TwoFactor/Block/Adminhtml/Index/Index.php
+++ Commerceshop/TwoFactor/Block/Adminhtml/Index/Index.php
@@ -5,13 +5,20 @@ namespace Commerceshop\TwoFactor\Block\Adminhtml\Index;
 class Index extends \Magento\Backend\Block\Widget\Container
 {
 
+protected $_backendUrl;
 
-
-    public function __construct(\Magento\Backend\Block\Widget\Context $context,array $data = [])
+    public function __construct(\Magento\Backend\Block\Widget\Context $context,array $data = [],
+    	\Magento\Backend\Model\UrlInterface $backendUrl)
     {
-        parent::__construct($context, $data);
+    	parent::__construct($context, $data);
+
+    	$this->_backendUrl = $backendUrl;
     }
+    public function getformurl()
+    {
+        // $params = array('some'=>'url_parameters');
 
+        return $url = $this->_backendUrl->getUrl("twofactor/index/verify");
 
-
+    }
 }
