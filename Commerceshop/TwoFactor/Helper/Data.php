8eb4cd7a467f06bc7ae133d9a990ea0a68904219
sasikiran <sasikiran.k@innoppl.com>
2017-02-16 14:58:01 +0530

    Added APIS

--- Commerceshop/TwoFactor/Helper/Data.php
+++ Commerceshop/TwoFactor/Helper/Data.php
@@ -6,11 +6,62 @@ namespace Commershop\TwoFactor\Helper;
 class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
 
+
+	protected $_autoloaderRegistered;
+    protected $_bitpay;
+    protected $_sin;
+    protected $_publicKey;
+    protected $_privateKey;
+    protected $_keyManager;
+    protected $_client;
+    protected $_bitpayModel;
+    protected $_directory_list;
+    protected $logger;
+    protected $config;
+
+
 	/**
      * @param \Magento\Framework\App\Helper\Context $context
      */
-	public function __construct(\Magento\Framework\App\Helper\Context $context
-	) {
-		parent::__construct($context);
+	// public function __construct(\Magento\Framework\App\Helper\Context $context
+	// ) {
+	// 	parent::__construct($context);
+	// }
+	public function __construct(
+		\Magento\Framework\App\Helper\Context $context,
+		\Magento\Framework\App\Filesystem\DirectoryList $directory_list,
+		\Bitpay\Core\Logger\Logger $logger,
+		\Magento\Config\Model\ResourceModel\Config $config)
+	{
+		
+		$this->_bitpayModel = $bitpayModel;
+        $this->directory_list = $directory_list;
+        $this->logger = $logger;
+        $this->config = $config;
+        parent::__construct($context);
 	}
+
+
+
+
+
+    public function registerAutoloader()
+    {
+        if (true === empty($this->_autoloaderRegistered)) {            
+            $base = $this->directory_list->getPath('app')."/code/Commerceshop/TwoFactor/lib/";
+            $autoloader_filename = $base.'way2sms-api.php';
+            if (true === is_file($autoloader_filename) && true === is_readable($autoloader_filename)) {
+                require_once $autoloader_filename;
+                \Bitpay\Autoloader::register();
+                $this->_autoloaderRegistered = true;
+                $this->debugData('[INFO] In \Bitpay\Core\Helper\Data::registerAutoloader(): autoloader file was found and has been registered.');
+            } else {
+                $this->_autoloaderRegistered = false;
+                $this->debugData('[ERROR] In \Bitpay\Core\Helper\Data::registerAutoloader(): autoloader file was not found or is not readable. Cannot continue!');
+                throw new \Exception('In \Bitpay\Core\Helper\Data::registerAutoloader(): autoloader file was not found or is not readable. Cannot continue!');
+            }
+        }
+    }
+
+
 }
--- Commerceshop/TwoFactor/Observer/AdminLoginSucceeded.php
+++ Commerceshop/TwoFactor/Observer/AdminLoginSucceeded.php
@@ -9,15 +9,18 @@ class AdminLoginSucceeded implements ObserverInterface
 	protected $_url;
 	protected $_authSession;
 	protected $_session;
+    protected $_directory_list;
 	public function __construct(\Magento\Framework\App\ResponseFactory $responseFactory,
 		\Magento\Backend\Model\Auth\Session $authSession,
 		\Magento\Framework\UrlInterface $url,
+        \Magento\Framework\App\Filesystem\DirectoryList $directory_list,
 		\Magento\Backend\Model\Session $session)
 	{	
 		$this->_responseFactory = $responseFactory;
 		$this->_url = $url;
 		$this->_authSession = $authSession;
 		$this->_session = $session;
+        $this->_directory_list = $directory_list;
         
 	}
     public function execute(Observer $observer) 
@@ -27,20 +30,13 @@ class AdminLoginSucceeded implements ObserverInterface
     	$auth->getUser()->getUserId();
     	$this->_session->setIsloggedin(1);
     	$this->_session->setOtpdone(0);
-
         $myValue = rand(111111,999999);
     	$this->_session->setOtp($myValue);
-    	// echo $this->_session->getSasi();
-
-    	// $writer = new \Zend\Log\Writer\Stream(BP . '/var/log/TwoFactor.log'); 
-    	// $logger = new \Zend\Log\Logger(); 
-    	// $logger->addWriter($writer); 
-    	// $logger->info($auth);
-    	// echo $auth->getUser()->getUserId();
-    	// exit();
-         //$this->redirect->redirect($controller->getResponse(), 'exampleadminnewpage/helloworld/index');
+        $base = $this->_directory_list->getPath('app')."/code/Commerceshop/TwoFactor/lib/";
+        require_once($base.'way2sms-api.php');
+        $message = "Your Verification code is : ".$myValue;
+        $mobileNumber = '9751537453';
+        if(sendWay2SMS ( '9789822842' , 'manoj' , $mobileNumber , $message)){
+        }
     }
-
-
-    
 }
