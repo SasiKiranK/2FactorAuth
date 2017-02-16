11c042b06fcf6bd423abb2dadd4789f56b918ed7
prathap <prathap.g@innoppl.com>
2017-02-16 15:23:21 +0530

    added resend controller

---
+++ Commerceshop/TwoFactor/Controller/Adminhtml/Index/Resend.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Commerceshop\TwoFactor\Controller\Adminhtml\Index;
+
+
+class Resend extends \Magento\Backend\App\Action
+{
+    public function execute()
+    {
+    	// echo "Error while processing OTP";
+    	// $this->_view->loadLayout();
+     //    $this->_view->getLayout()->initMessages();
+     //    $this->_view->renderLayout();
+    	/*echo "string";*/
+		// exit();
+	}
+}
+
+?>
